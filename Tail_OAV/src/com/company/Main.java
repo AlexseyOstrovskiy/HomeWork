dd0c09adebde4c14dd74467b6782c29b88319312
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2020-03-02 21:11:08 +0300

    02/03/2020

--- Tail_OAV/src/com/company/Main.java
+++ Tail_OAV/src/com/company/Main.java
@@ -1,22 +1,17 @@
 package com.company;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        //Files.lines(Paths.get("C:\\Users\\Asus\\Desktop\\Test.txt"), StandardCharsets.UTF_8).forEach(System.out::println);
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите путь к файлу");
         String way = sc.nextLine();
         Files.lines(Paths.get(way), StandardCharsets.UTF_8).forEach(System.out::println);
-
-
-
     }
 }
