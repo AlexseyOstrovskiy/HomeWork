3b0deee1c5743e6ba9484f8d70302022255751ae
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2020-04-04 22:39:07 +0300

    считает сколько раз повторяется каждая буква в заданной стоке

--- буквы/src/com/company/Main.java
+++ буквы/src/com/company/Main.java
@@ -3,26 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	String s = "sdssdaasddsa";
-	String[] arr = s.split("");
-        System.out.println(arr.length);
-        System.out.println(arr[0]);
-
-       
-       
-       
-
-
+        String s = "sdadfsfafdfsfafrgsvasddsa";
+        String [] arr = s.split("");
+        String[] arr2 = new String[arr.length];
         for (int k = 0; k < arr.length ; ) {
             int j =0;
             String a = arr[k];
-        for (int i = 0 ; i < arr.length ; i++) {
-           if( arr[i].equals(a)){
-            j++;
-           }
-        }
-            System.out.println( arr[k] +" " + j + " раз");
-        k++;}
-        }
+            for (int l = 0; l < arr2.length; l++) {
+            if(!arr[k].equals(arr2[l])){
+                if( arr[l].equals(a)){
+                    j++;
+                    arr2[l] = arr[l];
+                }
+            }}
+            if(j != 0){
+            System.out.println( arr[k] +" " + j + " раз");}
+            k++;}
+    }
     }
 
