2cc0761f6b1814baceac426455a8507b423625ca
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2020-05-02 10:30:45 +0300

    training 02.05.2020 simple numbers

--- training 01.05.2020/src/com/company/Main.java
+++ training 01.05.2020/src/com/company/Main.java
@@ -5,23 +5,24 @@ public class Main {
     public static void main(String[] args) {
         int x = 2;
         System.out.println("Простые числа из промежутка 2 - 100 : ");
-        for (int i = 3; i <101 ; i++) {
+
+        for (int i = 2; i <101 ; i++) {
+            if(i ==2){
+                System.out.println(i);
+            }
+            if(i == 3){
+                System.out.println(i);
+            }
+            if(i == 5){
+                System.out.println(i);
+            }
+            if(i == 7){
+                System.out.println(i);
+            }
             if( i % 2 != 0){
-                if(i == 2){
-                    System.out.println(i);
-                }
                 if( i % 3 != 0){
-                    if(i == 3){
-                        System.out.println(i);
-                    }
                     if( i % 5 != 0){
-                        if(i == 5){
-                            System.out.println(i);
-                        }
                         if( i % 7 != 0){
-                            if(i == 7){
-                                System.out.println(i);
-                            }
                             System.out.println(i);
                         }
                     }
