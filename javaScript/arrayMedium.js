3d1ca8b0063b81f567a0279a746e70b6c6ce209a
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2020-05-31 14:06:38 +0300

    js

--- javaScript/arrayMedium.js
+++ javaScript/arrayMedium.js
@@ -5,7 +5,7 @@ function getRAndomInt (min, max){
     max = Math.floor(max)
     return Math.floor(Math.random() * (max-min +1))+min;
 }
- console.log(getRAndomInt(1, 100));
+ //console.log(getRAndomInt(1, 100));
 
 // создание массива
 var myArr = [];
@@ -35,16 +35,66 @@ var value1 = prompt('Введите первое целое число');
 var value2 = prompt('Введите второе целое число');
 var value3 = prompt('Введите третье целое число');
 
-if(value1 > value2 & value1 >value3){
+if(value1 > value2 && value1 >value3){
     alert('Наибольшее число из введённых ' + value1);
 }
 
-if(value2 > value1 & value2 >value3){
+else if(value2 > value1 && value2 >value3){
     alert('Наибольшее число из введённых ' + value2);
 }
 
-if(value3 > value2 & value3 >value1){
-    alert('Наибольшее число из введённых ' + value3);
+//if(value3 > value2 && value3 >value1){
+  else(alert('Наибольшее число из введённых ' + value3));
+//}
+
+if(value1 == value2 & value2 == value3){
+    alert('Все введённые числа равны между собой.')
+};
+
+console.log("***Задача 3***");
+
+function getRAndomInt (min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max-min +1))+min;
+}
+let randomNum = getRAndomInt(1,101);
+console.log(randomNum);
+alert('Игра. Тебе нужно угадать число.');
+
+let userNum = prompt('Введите число от 1 до 100');
+let meter = 0;
+
+if(userNum < 1 || userNum >100){
+    alert('Число должно быть в передах 1 -100!')
+} 
+
+if(userNum == randomNum){
+    alert('Поздравляю! вы угадали!')
+}
+
+//if(userNum !=0){
+while(userNum != randomNum){
+ if(userNum > randomNum){
+     meter++;
+     userNum = prompt('Меньше!',0)
+    //alert('Меньше.')
+
 }
 
 
+if(userNum < randomNum){
+    meter++;
+    userNum = prompt('Больше!',0)
+    //alert('Больше')
+}
+
+//if(userNum == 0){
+  //  alert('Введите число от 1 до 100!');
+//}
+
+}
+alert('Поздравляю, Вы угадали! Вам потребовалось ' + meter + 'попыток.' )
+//}
+//else(
+    //alert('Вы ничего не ввели!')
