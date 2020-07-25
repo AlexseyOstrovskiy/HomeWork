72bd223333fa09f02decdaca1fc300b042bb20b0
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2020-07-25 23:52:31 +0300

    new

--- javaScript/Countries v2/main.js
+++ javaScript/Countries v2/main.js
@@ -1,3 +1,4 @@
+
 let arr = [];
 const requestURL = 'https://restcountries.eu/rest/v2/all'
 function sendRequest(){   
@@ -14,6 +15,7 @@ xhr.onerror=()=>{
 xhr.send()
 })
 }
+
 sendRequest('GET', requestURL)
 .then(data=> console.log(data)) 
 .catch(err => console.log(err))
@@ -110,4 +112,98 @@ FLAG[i].appendChild(FLAG_FLAG[i]);
 },
  3000);
 
+ //input
+let btn;
+btn = document.getElementById('btn');
+let search;
+btn.onclick = function(){
+    search = document.getElementById('inputID').value;
+    alert(search);
+    cardHolder.style.display = "none";
+/*
+    let x;
+    for(let i = 0; i <arr.length; i++){
+        if(arr[i].name = search){
+        x = i;
+        }
+    }
+    */
+   // console.log(arr[x].name + arr[x].population + arr[x].flag);
 
+/*
+    let NAME1;
+    let POPULATION1;
+    let FLAG1;
+    let FLAG_FLAG1;
+
+document.body.appendChild(mainCard('card'));
+document.getElementById("card").appendChild(name('name'));
+document.getElementById("card").appendChild(flag('flag'));
+document.getElementById("card").appendChild(population('population'));
+
+NAME1 = document.getElementById('name');
+NAME1.innerText = arr[x].name;
+
+POPULATION1 = document.getElementById('population');
+POPULATION1.innerText = "Население = " + arr[x].population + "человек.";
+
+
+FLAG1 = document.getElementById('flag');
+FLAG_FLAG1 = document.createElement("img");
+
+FLAG_FLAG1.src = arr[x].flag;
+FLAG_FLAG1.height = 100;
+FLAG_FLAG1.width = 240;
+FLAG1.appendChild(FLAG_FLAG1);
+   */
+  
+   function getCountry(countryName) {
+    return new Promise((resolve,reject) => {
+        $.ajax({
+               url: 'https://restcountries.eu/rest/v2/name/' + countryName,
+           success(data) {
+               resolve(data);
+           },
+           error(err) {
+               reject(err);
+           }
+       });   
+   });
+} 
+
+    //requestURL2 = 'https://restcountries.eu/rest/v2/name/' + a
+    //sendRequest('GET', requestURL2)
+    //return a;
+ }
+ 
+
+//let b = btn.onclick -5;
+//console.log(b);
+//alert(b);
+
+/*console.log("*****************");
+
+let arr2 = [];
+const requestURL3 = 'https://restcountries.eu/rest/v2/currency/cop'
+function sendRequest(){   
+    return new Promise((resolve, reject)=>{
+const xhr = new XMLHttpRequest();
+xhr.open('GET', requestURL3)
+xhr.onload = () =>{
+    console.log(JSON.parse(xhr.response))
+    arr = JSON.parse(xhr.response);
+}
+xhr.onerror=()=>{
+    console.log(xhr.response)
+}
+xhr.send()
+})
+}
+
+sendRequest('GET', requestURL)
+.then(data=> console.log(data)) 
+.catch(err => console.log(err))
+
+
+console.log("*****************");
+*/
