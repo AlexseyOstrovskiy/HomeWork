0415e291ca3ba08bbd725352b742c4132e41e83b
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2020-10-12 17:00:40 +0300

    YouTube TASK

--- MusicPlayer/src/main/java/org.Alex/MusicPlayer.java
+++ MusicPlayer/src/main/java/org.Alex/MusicPlayer.java
@@ -1,12 +1,62 @@
 package org.Alex;
 
-public class MusicPlayer {
-    private Music music;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicPlayer {
+
+
+    private List<Music> musicList = new ArrayList<>();
+//    ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext(
+//            "applicationContext.xml"
+//    );
+
+    private String name;
+    private int volume;
+//    private Music music;
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+//    public  MusicPlayer(){}
+//
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+
     public void playMusic(){
-        System.out.println("Now play " + music.getSong());
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+           "applicationContext.xml"
+   );
+        musicList.add(context.getBean("musicBeanRock", RockMusic.class));
+        musicList.add(context.getBean("musicBeanClassic", ClassicalMusic.class));
+        musicList.add(context.getBean("musicBeanRap", RapMusic.class));
+
+        for (int i = 0; i <musicList.size() ; i++) {
+        System.out.println("Now play " +musicList.get(i).getSong());
+        }
+        context.close();
     }
 }
--- MusicPlayer/src/main/java/org.Alex/TestSpring.java
+++ MusicPlayer/src/main/java/org.Alex/TestSpring.java
@@ -1,6 +1,7 @@
 //https://www.youtube.com/watch?v=Ns0IxBXDbWw&list=PLAma_mKffTOR5o0WNHnY0mTjKxnCgSXrZ&index=4
 package org.Alex;
 
+import com.sun.deploy.net.MessageHeader;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
@@ -8,9 +9,15 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("musicBean", Music.class);
-       MusicPlayer musicPlayer = new MusicPlayer(music);
+
+
+
+//        Music music = context.getBean("musicBean", Music.class);
+//       MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
        musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
        context.close();
 
     }
