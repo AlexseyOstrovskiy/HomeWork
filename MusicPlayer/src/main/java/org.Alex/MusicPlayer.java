8cd05a80db8771a0fd008957cfe1192269fad17d
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2020-10-19 20:16:07 +0300

    musicPlayer

--- MusicPlayer/src/main/java/org.Alex/MusicPlayer.java
+++ MusicPlayer/src/main/java/org.Alex/MusicPlayer.java
@@ -6,20 +6,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MusicPlayer {
-
-
-    private List<Music> musicList = new ArrayList<>();
-//    ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext(
-//            "applicationContext.xml"
-//    );
-
     private String name;
     private int volume;
-//    private Music music;
+    private Music music;
+
+//    private List<Music>musics;
+//    public  MusicPlayer(List<Music>musics){
+//    this.musics= musics;
+//    }
+//
+
+    private List<Music>musicList = new ArrayList<>();
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
 
     public void setMusicList(List<Music> musicList) {
         this.musicList = musicList;
     }
+
+
+
 
     public int getVolume() {
         return volume;
@@ -37,26 +45,45 @@ public class MusicPlayer {
         this.name = name;
     }
 
-//    public MusicPlayer(Music music) {
-//        this.music = music;
-//    }
-//    public  MusicPlayer(){}
-//
-//    public void setMusic(Music music) {
-//        this.music = music;
-//    }
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+    public  MusicPlayer(){}
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
 
     public void playMusic(){
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//           "applicationContext.xml"
+//   );
+//        musicList.add(context.getBean("musicBeanRock", RockMusic.class));
+//        musicList.add(context.getBean("musicBeanClassic", ClassicalMusic.class));
+//        musicList.add(context.getBean("musicBeanRap", RapMusic.class));
+//
+//        for (int i = 0; i <musicList.size() ; i++) {
+//        System.out.println("Now play " +musicList.get(i).getSong());
+//        }
+//
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-           "applicationContext.xml"
-   );
-        musicList.add(context.getBean("musicBeanRock", RockMusic.class));
-        musicList.add(context.getBean("musicBeanClassic", ClassicalMusic.class));
-        musicList.add(context.getBean("musicBeanRap", RapMusic.class));
+                "applicationContext.xml"
+        );
+            ClassicalMusic classicalMusic = context.getBean("musicBeanClassic", ClassicalMusic.class);
+            RapMusic rapMusic = context.getBean("musicBeanRap", RapMusic.class);
+            RockMusic rockMusic = context.getBean("musicBeanRock", RockMusic.class);
+            musicList.add( classicalMusic);
+            musicList.add(rockMusic);
+            musicList.add(rapMusic);
 
         for (int i = 0; i <musicList.size() ; i++) {
-        System.out.println("Now play " +musicList.get(i).getSong());
+            System.out.println(musicList.get(i).getSong());
         }
-        context.close();
+        System.out.println("Volume " + getVolume());
+        System.out.println("Name this player " + getName());
+       //System.out.println(musicList.size());
+
+//        System.out.println(music.getSong());
+
     }
 }
--- MusicPlayer/src/main/java/org.Alex/TestSpring.java
+++ MusicPlayer/src/main/java/org.Alex/TestSpring.java
@@ -12,12 +12,13 @@ public class TestSpring {
 
 
 
-//        Music music = context.getBean("musicBean", Music.class);
-//       MusicPlayer musicPlayer = new MusicPlayer(music);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//       MusicPlayer musicPlayer = new MusicPlayer(music);
+      //  MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
        context.close();
 
     }
